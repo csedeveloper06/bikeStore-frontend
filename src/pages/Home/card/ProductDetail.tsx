a2f00ad0c0233e9203d331b0ac182e8821757b87
unknown <you@example.com>
2025-03-12 04:13:40 +0600

    added related products

--- src/pages/Home/card/ProductDetail.tsx
+++ src/pages/Home/card/ProductDetail.tsx
@@ -2,8 +2,12 @@ import { useParams } from "react-router-dom";
 import ReactSlickExample from "../../../components/ReactSlick";
 import "../../../styles/examples.css";
 import { useEffect, useState } from "react";
+import { useGetProductsQuery } from "../../../redux/api/baseApi";
+import ProductCard from "./ProductCard";
 
 const ProductDetail = () => {
+  const { data } = useGetProductsQuery(undefined);
+
   const { productId } = useParams();
   console.log(productId);
   const [product, setProduct] = useState(null);
@@ -23,43 +27,63 @@ const ProductDetail = () => {
     fetchProduct();
   }, [productId]);
   console.log(product);
-  const { images } = product || {};
-  // const { images } = product;
-  // console.log(images);
+
+  const {
+    name,
+    brand,
+    price,
+    images,
+    category,
+    description,
+    quantity,
+    inStock,
+  } = product || {};
+
+  const relatedProducts = data?.data?.filter(
+    (relatedProduct) => relatedProduct.category === category
+  );
+  console.log("Related Products are :", relatedProducts);
 
   return (
-    <div className="fluid react-slick">
-      <div className="fluid__image-container">
-        <ReactSlickExample
-          {...{ images }}
-          {...{
-            rimProps: {
-              isEnlargedImagePortalEnabledForTouch: false,
-              enlargedImagePortalId: "portal",
-              enlargedImageContainerDimensions: {
-                width: "200%",
-                height: "100%",
+    <div>
+      <div className="fluid react-slick">
+        <div className="fluid__image-container">
+          <ReactSlickExample
+            rsProps={undefined}
+            {...{ images }}
+            {...{
+              rimProps: {
+                isEnlargedImagePortalEnabledForTouch: false,
+                enlargedImagePortalId: "portal",
+                enlargedImageContainerDimensions: {
+                  width: "200%",
+                  height: "100%",
+                },
               },
-            },
-          }}
-        />
+            }}
+          />
+        </div>
+        <div className="fluid__instructions" style={{ position: "relative" }}>
+          <div id="portal" className="portal" />
+          <h1>Product Detailed Information</h1>
+          <h3>Name : {name}</h3>
+          <h3>Brand : {brand}</h3>
+          <h3>Price : {price}</h3>
+          <p>Product Description{description}</p>
+          <h3>Category : {category}</h3>
+          <h3>Quantity : {quantity}</h3>
+          <h3>InStock : {inStock ? "Available" : "Not Available"}</h3>
+          <button className="btn btn-soft btn-outline btn-success">
+            Buy Now
+          </button>
+        </div>
+        <div style={{ height: "400px" }} />
       </div>
-      <div className="fluid__instructions" style={{ position: "relative" }}>
-        <div id="portal" className="portal" />
-        <h3>External Enlarged Image Example</h3>
-        <p>Render enlarged image into an HTML element of your choosing.</p>
-        <p>
-          Ignored for touch input by default but will be honored if
-          isEnlargedImagePortalEnabledForTouch is implemented.
-        </p>
-        <p>
-          Use cases include a scenario where an ancestor element of
-          react-image-magnify implements overflow hidden.
-        </p>
-        <p>Requires React v16.</p>
-        <p>Please see for details.</p>
+      <div className="grid grid-cols-5 gap-4">
+        {relatedProducts?.map((relatedProduct) => (
+          <ProductCard product={relatedProduct} key={relatedProduct.id} />
+        ))}
       </div>
-      <div style={{ height: "1000px" }} />
     </div>
   );
 };
